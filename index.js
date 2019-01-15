438dbad58028d0c6cca592a26af50e3c9868f1e9
Pia Lyck Festersen <pialyck@gmail.com>
2019-01-15 21:39:34 +0100

    Initial commit - with Express and ESLint

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.listen(8003, () => {
+    console.log('Example app listening on port 8003!')
+});
